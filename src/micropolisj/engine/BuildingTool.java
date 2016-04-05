fd28e7f178551f1767fad4253289235e325af0d8
cloud7funez <cloud7funez@gmail.com>
2016-04-05 05:10:59 -0400

    first upload

--- src/micropolisj/engine/BuildingTool.java
+++ src/micropolisj/engine/BuildingTool.java
@@ -51,6 +51,9 @@ class BuildingTool extends ToolStroke
 
 		case AIRPORT:
 			return applyZone(eff, AIRPORT);
+			
+		case AIRFILTER:
+			return applyZone(eff, AIRFILTER);
 
 		default:
 			// not expected
--- src/micropolisj/engine/MicropolisTool.java
+++ src/micropolisj/engine/MicropolisTool.java
@@ -29,7 +29,8 @@ public enum MicropolisTool
 	POWERPLANT(4, 3000),
 	NUCLEAR(4, 5000),
 	AIRPORT(6, 10000),
-	QUERY(1, 0);
+	QUERY(1, 0),
+	AIRFILTER(3, 3000);
 
 	int size;
 	int cost;
@@ -68,6 +69,7 @@ public enum MicropolisTool
 		case POWERPLANT:
 		case NUCLEAR:
 		case AIRPORT:
+		case AIRFILTER:
 			return new BuildingTool(engine, this, xpos, ypos);
 
 		default:
